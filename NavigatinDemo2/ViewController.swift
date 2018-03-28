b97166f5af2904b8dc9743cf13deeedc4233bcbc
Daniil Poplavski <daniilpopl1111@gmail.ocm>
2018-03-29 00:05:25 +0300

    New test application

---
+++ NavigatinDemo2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NavigatinDemo2
+//
+//  Created by Daniil Poplavski on 29.03.2018.
+//  Copyright © 2018 Daniil Poplavski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
